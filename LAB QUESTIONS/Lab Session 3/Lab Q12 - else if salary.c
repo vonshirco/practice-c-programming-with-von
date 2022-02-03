dc18784ceb8796c7679f46f87bf7aa7f72709445
Von <vonshirco.vs@gmail.com>
2022-02-04 01:14:02 +0300

    Updated Lab Q12

--- LAB QUESTIONS/Lab Session 3/Lab Q12 - else if salary.c
+++ LAB QUESTIONS/Lab Session 3/Lab Q12 - else if salary.c
@@ -8,27 +8,27 @@ main(){
 	scanf("%f",&salary);
 	
 	if(salary <= 170000){
-		relTax = 0 * salary; //0% = 0/100
+		relTax = 0 * salary; 
 		printf("Your Relevant Tax is %.2f", relTax);
 	}
 		
 	else if(salary > 170000 && salary <= 360000){
-		relTax = 0.11 * salary; //11% = 11/100
+		relTax = 0.11 * salary; //11% = 11/100 = 0.11
 		printf("Your Relevant Tax is %.2f", relTax);
 	}
 		
 	else if(salary > 360000 && salary <= 540000){
-		relTax = 0.2 * salary; //20% = 20/100
+		relTax = 0.2 * salary;
 		printf("Your Relevant Tax is %.2f", relTax);
 	}
 	
 	else if(salary > 540000 && salary <= 720000){
-		relTax = 0.25 * salary; //25% = 25/100
+		relTax = 0.25 * salary; 
 		printf("Your Relevant Tax is %.2f", relTax);
 	}
 		
 	else{ //if salary > 720000
-		relTax = 0.3 * salary; //30% = 30/100
+		relTax = 0.3 * salary; 
 		printf("Your Relevant Tax is %.2f", relTax);
 	}
 		

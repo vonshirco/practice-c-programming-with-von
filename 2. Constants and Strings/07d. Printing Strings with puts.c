4e8d2310f12664be86e43c5040252986af4ec03d
Von <vonshirco.vs@gmail.com>
2022-02-10 13:10:31 +0300

    Created a file

---
+++ 2. Constants and Strings/07d. Printing Strings with puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main(){
+	//Puts - the same as printf
+	
+	char name[15];
+	
+	printf("Your Name: ");
+	gets(name); //does the same as scanf
+	
+	puts(name); //PRINTS TO SCREEN same as printf
+	
+}

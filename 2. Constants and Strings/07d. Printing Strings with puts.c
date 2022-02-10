fb0ca76e51bda20c18f527dade0a5f790c50f7b5
Von <vonshirco.vs@gmail.com>
2022-02-10 13:41:08 +0300

    Update file

--- 2. Constants and Strings/07d. Printing Strings with puts.c
+++ 2. Constants and Strings/07d. Printing Strings with puts.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 main(){
-	//Puts - the same as printf
+	//Puts - the same as printf, it does away with the limitations of the printf(terminating string after reading a blank space)
 	
 	char name[15];
 	
